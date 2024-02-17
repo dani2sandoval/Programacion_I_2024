1b23a7d3c0bce20c4bff52c420844c0704d58ff8
dani2sandoval <msandovalm15@miumg.edu.gt>
2024-02-17 10:52:24 -0600

    practicapoo y preguntas.txt

---
+++ practicapoo/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace práctica_poo
+{
+    public class Figurita
+    {
+        public virtual void Dibujar()
+        {
+            Console.WriteLine("Dibujando una figura");
+        }
+    }
+
+    public class Triangulo : Figurita
+    {
+        public override void Dibujar()
+        {
+            Console.WriteLine("Dibujando un triangulo");
+        }
+    }
+
+    public class Circulo : Figurita
+    {
+        public override void Dibujar()
+        {
+            Console.WriteLine("Dibujando un círculo");
+        }
+    }
+
+    class Programa
+    {
+        static void Main()
+        {
+            Triangulo miTriangulo = new Triangulo();
+            Circulo miCirculo = new Circulo();
+
+            Console.WriteLine("Figurita genérica:");
+            DibujarFigura(new Figurita());
+
+            Console.WriteLine("\nCuadrado:");
+            DibujarFigura(miTriangulo);
+
+            Console.WriteLine("\nCírculo:");
+            DibujarFigura(miTriangulo);
+        }
+
+        static void DibujarFigura(Figurita figura)
+        {
+            figura.Dibujar();
+        }
+    }
+
+}
