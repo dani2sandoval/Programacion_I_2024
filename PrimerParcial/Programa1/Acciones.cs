e684240a2d8c6bdc3d00a87f7519eddb7e17325e
dani2sandoval <msandovalm15@miumg.edu.gt>
2024-03-02 10:26:06 -0600

    commit del parcial

---
+++ PrimerParcial/Programa1/Acciones.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa1
+{
+    internal class Acciones
+    {
+       
+        public int saldo { get; set; }
+        public int cantidadadepositar { get; set; }
+
+        public int cantidadaretirar { get; set; }
+
+        public string opcion {  get; set; }
+
+       public void buclemenu()
+       {
+
+            saldo = 1500;
+            int x = 1;
+             
+            do
+            {
+                Console.WriteLine("Bienvenido a tu banca virtual");
+                Console.WriteLine("a. Ver saldo actual");
+                Console.WriteLine("b. Depositar en la cuenta");
+                Console.WriteLine("c. Retirar dinero de la cuenta");
+                Console.WriteLine("d. Salir del programa");
+                opcion = Console.ReadLine();
+
+
+
+                switch (opcion)
+                {
+                    case "a":
+                        Console.WriteLine("Su saldo es de " + saldo);
+                        break;
+                    
+                    case "b":
+                        Console.WriteLine("Cuanto desea depositar: ");
+                        cantidadadepositar = int.Parse(Console.ReadLine());
+                        saldo = cantidadadepositar + saldo;
+                        Console.WriteLine("deposito realizado");
+                        break;
+
+                    case "c":
+                        Console.WriteLine("Cuanto desea retirar: ");
+                        cantidadaretirar = int.Parse(Console.ReadLine());
+                        saldo = saldo - cantidadaretirar;
+                        Console.WriteLine("retiro realizado");
+                        break;
+
+                    case "d":
+                        x = 0;
+                        break;
+
+                    default:
+                        Console.WriteLine("ingrese una opcion correcta");
+                        break ;
+                        
+                }
+                
+            } while (x == 1);
+
+            
+       }
+
+
+    }
+}
---
+++ PrimerParcial/Programa2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Programa2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            int x = 1;
+            int sum;
+            int prom = 0;
+            do
+            {
+                Console.Write("Ingrese un número: ");
+                sum = int.Parse(Console.ReadLine());
+
+                if (sum != 0)
+                {
+                    num  += sum;
+                    prom++;
+                }
+                else
+                {
+                    x = 0;
+                }
+
+            } while (x == 1);
+
+            Console.WriteLine("la suma de todos los numeros es: " + num);
+            Console.WriteLine("El promedio es de: " + num/prom);
+            Console.ReadLine();
+
+
+
+
+            
+
+
+        }
+    }
+}
